72cc947aa5f062d37dff7332c632ef41bab8a993
Joyce Lin <jlin@blackducksoftware.com>
2017-11-16 15:44:02 -0500

    Update ExitCodeListener.java

--- src/main/java/org/testng/reporters/ExitCodeListener.java
+++ src/main/java/org/testng/reporters/ExitCodeListener.java
@@ -6,6 +6,7 @@ import org.testng.TestNG;
  * A very simple <code>ITestListener</code> used by the TestNG runner to
  * find out the exit code.
  *
+ *
  * @author <a href='mailto:the_mindstorm[at]evolva[dot]ro'>Alexandru Popescu</a>
  */
 public class ExitCodeListener extends TestNG.ExitCodeListener {
